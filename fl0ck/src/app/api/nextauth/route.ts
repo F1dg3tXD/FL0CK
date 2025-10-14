6d79c4d77befc23982cd37d855d9cbf96907f8f6
F1dg3t <57208077+F1dg3tXD@users.noreply.github.com>
2025-10-13 23:22:32 -0700

    Update route.ts

--- fl0ck/src/app/api/nextauth/route.ts
+++ fl0ck/src/app/api/nextauth/route.ts
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
-})
-
-export { handler as GET, handler as POST }
+}
+export default NextAuth(authOptions)
